c5b651ee84be7afdad476c45e076fa7580b417b3
vibesx <lucian.paslariu@gmail.com>
2022-11-08 15:10:18 +0200

    implemented redux toolkit

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,5 +1,6 @@
 import { useSelector, useDispatch } from "react-redux";
 
+import { counterActions } from "../store/counter-store";
 import classes from "./Counter.module.css";
 
 const Counter = () => {
@@ -8,20 +9,43 @@ const Counter = () => {
 	const show = useSelector((state) => state.showCounter);
 
 	const incrementHandler = () => {
-		dispatch({ type: "increment" });
+		// we need to call the function in these cases because it returns an action object with type and everything else we need
+		dispatch(counterActions.increment());
 	};
 
 	const increaseHandler = () => {
-		dispatch({ type: "increase", amount: 5 });
+		dispatch(counterActions.increase(5));
+		// what this function returns (with arguments) is an object of the type:
+		// {type: SOME_ID, payload: <what_we_entered_as_arguments>}
+		// payload is a fixed name, that is why we need to manipulate what we want to pass in the arguments stage
+		// if we only want one value, ex a number, we can pass it and call it using action.payload
+		// if we need multiple values, we should pass an object
 	};
 
 	const decrementHandler = () => {
-		dispatch({ type: "decrement" });
+		dispatch(counterActions.decrement());
 	};
 
 	const toggleCounterHandler = () => {
-		dispatch({ type: "toggle" });
+		dispatch(counterActions.toggleCounter());
 	};
+
+	// old non-toolkit approach
+	// const incrementHandler = () => {
+	// 	dispatch({ type: "increment" });
+	// };
+
+	// const increaseHandler = () => {
+	// 	dispatch({ type: "increase", amount: 5 });
+	// };
+
+	// const decrementHandler = () => {
+	// 	dispatch({ type: "decrement" });
+	// };
+
+	// const toggleCounterHandler = () => {
+	// 	dispatch({ type: "toggle" });
+	// };
 
 	return (
 		<main className={classes.counter}>
--- src/store/counter-store.js
+++ src/store/counter-store.js
@@ -1,29 +1,84 @@
-import { createStore } from "redux";
+// import { createStore } from "redux";
+import { createSlice, configureStore } from "@reduxjs/toolkit";
 
-const initialState = { counter: 0, showCounter: true };
+const initialCounterState = { counter: 0, showCounter: true };
+const initialAuthState = { isAuthenticated: false };
 
-const counterReducer = (state = initialState, action) => {
-	if (action.type === "increment") {
-		return { counter: state.counter + 1, showCounter: state.showCounter };
-	}
-	if (action.type === "increase") {
-		return {
-			counter: state.counter + action.amount,
-			showCounter: state.showCounter,
-		};
-	}
-	if (action.type === "decrement") {
-		return { counter: state.counter - 1, showCounter: state.showCounter };
-	}
-	if (action.type === "toggle") {
-		return {
-			showCounter: !state.showCounter,
-			counter: state.counter,
-		};
-	}
-	return state;
-};
+const counterSlice = createSlice({
+	name: "counter",
+	initialCounterState,
+	reducers: {
+		increment(state) {
+			console.log("ASDASDAS");
+			// we should never manipulate state like this usually, but createSlice has a built-in functionality
+			// that detects these kind of manipulations and actually makes copies instead of modifying the existing state
+			state.counter++;
+		},
+		decrement(state) {
+			state.counter--;
+		},
+		increase(state, action) {
+			state.counter += action.payload;
+		},
+		toggleCounter(state) {
+			console.log(state.showCounter);
 
-const store = createStore(counterReducer);
+			state.showCounter = !state.showCounter;
+		},
+	},
+});
 
+const authSlice = createSlice({
+	name: "auth",
+	initialAuthState,
+	reducers: {
+		login(state) {
+			state.isAuthenticated = true;
+		},
+		logout(state) {
+			state.isAuthenticated = false;
+		},
+	},
+});
+
+// we can access actions as below:
+// counterSlice.actions.increment;
+// this will return an action object of this shape: {type: "some auto-generated uinque id"}
+
+const store = configureStore({
+	// reducer can get a single reducer, like counterSlice.reducer, or multiple reducers combined using combineReducers()
+	// docs: https://redux.js.org/api/combinereducers
+	reducer: counterSlice.reducer,
+});
+
+// this is an option to work with slices, but configureStore from the toolkit is recommended
+//const store = createStore(counterSlice.reducer);
+
+/* classic, non-toolkit approach */
+
+// const counterReducer = (state = initialState, action) => {
+// 	if (action.type === "increment") {
+// 		return { counter: state.counter + 1, showCounter: state.showCounter };
+// 	}
+// 	if (action.type === "increase") {
+// 		return {
+// 			counter: state.counter + action.amount,
+// 			showCounter: state.showCounter,
+// 		};
+// 	}
+// 	if (action.type === "decrement") {
+// 		return { counter: state.counter - 1, showCounter: state.showCounter };
+// 	}
+// 	if (action.type === "toggle") {
+// 		return {
+// 			showCounter: !state.showCounter,
+// 			counter: state.counter,
+// 		};
+// 	}
+// 	return state;
+// };
+
+// const store = createStore(counterReducer);
+
+export const counterActions = counterSlice.actions;
 export default store;
